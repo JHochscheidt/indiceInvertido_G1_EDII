332e4a12ac067ae37c802a9f2876cd51aa37b76f
Jackson Hochscheidt <jackson94h@gmail.com>
2015-09-26 16:10:32 -0300

    criado prototipo de estrutura para lista

--- indiceInvertido.h
+++ indiceInvertido.h
@@ -1,24 +1,31 @@
 #ifndef _INDICE_INVERTIDO_H
-#define indiceInvertido.h
+#define indiceInvertido
 
 #define RED 1
 #define BLACK 0
 
 typedef struct _nodo{
-	char *info;
 	struct _nodo *pai;
 	struct _nodo *esq;
 	struct _nodo *dir;
 	int cor;
-}
+	char *info; // palavra a ser indexada
+	struct _lista *ocorrencias; // ponteiro para a lista com os arquivos em que a palavra (info) ocorre	
+}NODO;
 
-void criaArvore(){
-	
-}
+typedef struct _lista{
+	struct _lista *ant;
+	struct _lista *prox;
+	int pos;
+}LISTA;
+// definindo valores do nodo NULL
 
-void *criaNodoNull(){
-	
-}
+NODO *criaNodoNull();
+LISTA *criaListaVazia();
+
+NODO *insert();
+NODO *delete();
+void verificaArvore(NODO *root);
 
 
 
