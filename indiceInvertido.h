9138b407cb232e7ba8562786bd1e60b740357cd4
Jackson Hochscheidt <jackson94h@gmail.com>
2015-09-26 14:56:37 -0300

    arquivo cabecalho da implementacao do indice invertido. Definida estrutura de um nodo

---
+++ indiceInvertido.h
@@ -0,0 +1,30 @@
+#ifndef _INDICE_INVERTIDO_H
+#define indiceInvertido.h
+
+#define RED 1
+#define BLACK 0
+
+typedef struct _nodo{
+	char *info;
+	struct _nodo *pai;
+	struct _nodo *esq;
+	struct _nodo *dir;
+	int cor;
+}
+
+void criaArvore(){
+	
+}
+
+void *criaNodoNull(){
+	
+}
+
+
+
+
+
+
+
+
+#endif
