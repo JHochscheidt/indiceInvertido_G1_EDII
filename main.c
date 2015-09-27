ff58c645a4789a9f139dab85de6c43e9d5a02d69
Jackson Hochscheidt <jackson94h@gmail.com>
2015-09-26 22:06:33 -0300

    arquivo fonte com a main

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "indiceInvertido.h"
+
+
+int main(int argc, char **argv){
+
+
+		// nodo null
+		NODO *NIL;
+		NIL= criaNodoNull();
+
+		// criando arvore
+		NODO *root = criaArvoreVazia();
+
+		insert("abacate", root);
+		
+
+
+
+}
