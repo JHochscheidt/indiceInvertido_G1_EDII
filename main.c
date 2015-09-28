f331f0b6566717d2abf6ca06dd16e6a17049529e
Jackson Hochscheidt <jackson94h@gmail.com>
2015-09-28 14:52:09 -0300

    mais testes

--- main.c
+++ main.c
@@ -9,7 +9,35 @@ int main(int argc, char **argv){
 		TpNodo *arv = NULL;
 		
 
-		arv = insert("abacate", arv);
+		arv = insert("m", arv);
+		arv = insert("f", arv);
+		arv = insert("c", arv);
+		arv = insert("b", arv);
+		arv = insert("a", arv);
+		arv = insert("d", arv);
+		arv = insert("e", arv);
+		arv = insert("i", arv);
+		arv = insert("k", arv);
+		arv = insert("l", arv);
+		arv = insert("j", arv);
+		arv = insert("g", arv);
+		arv = insert("h", arv);
+		arv = insert("t", arv);
+		arv = insert("p", arv);
+		arv = insert("r", arv);
+		arv = insert("s", arv);
+		arv = insert("q", arv);
+		arv = insert("o", arv);
+		arv = insert("n", arv);
+		arv = insert("w", arv);
+		arv = insert("y", arv);
+		arv = insert("x", arv);
+		arv = insert("z", arv);
+		arv = insert("u", arv);
+		arv = insert("v", arv);
+		
+		
+		
 		Desenha(arv,0);
 
 
