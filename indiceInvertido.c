78116d6cda4684b929fc94b2f7dc972709419f11
Jackson Hochscheidt <jackson94h@gmail.com>
2015-09-28 14:51:18 -0300

    funcao inserir pronta, agora necessita fazer a verificacao da arvore, para recolorir os nos, apos a insercao

--- indiceInvertido.c
+++ indiceInvertido.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include "indiceInvertido.h"
 
+// sentinela nodoNULL
+static TpNodo *nodoNULL = NULL;
+
 TpNodo *insert(char *palavra, TpNodo *root){
 	// se arvore estiver vazia
 	if(root == NULL){
@@ -26,7 +29,51 @@ TpNodo *insert(char *palavra, TpNodo *root){
 		puts("inserido primeira chave, RAIZ\n\n");
 		return root;
 	}
+	// arvore nao vazia
+	TpNodo *atual = root;
+	TpNodo *pai = atual->pai;
+	//TpNodo *avo = pai->pai;
+	int cmp ; // para pegar o retorno da funcao strcmp
+	while(atual != nodoNULL){
+	//	avo = pai;
+		pai = atual;
+		cmp = strcmp(palavra, atual->info);
+		if(cmp < 0) // palavra é menor que o atual
+			atual = atual->esq;
+		else if(cmp > 0) // palavra é maior que o atual
+			atual = atual->dir;
+		else{
+			// palavra ja esta inserida.
+			// deve-se buscar dentro da lista do nodo atual a posicao para a palavra
+		}
+	} // fim while percorre arvore
+
+		// lembrar de faze a insercao caso a palavra ja exista - OBS verificar while no ELSE seria onde cai neste caso
+
+		
+		atual = (TpNodo*) malloc(sizeof(TpNodo));
+		atual->info = (char*) malloc(sizeof(strlen(palavra)));
+		strcpy(atual->info, palavra);
+		atual->cor = RED;
+		atual->esq = atual->dir = nodoNULL;
+		atual->pai = pai;
+		// verificar se o pai->info < || > atual->info
+		cmp = strcmp(atual->info,pai->info);
+		if(cmp < 0) // atual é menor que pai->info
+			pai->esq = atual;
+		else pai->dir = atual;
+	}
+
+
+	puts("nodo inserido\n\n");
+
 	return root;
+	
+	
+
+
+
+	
 }
 
 void Desenha(TpNodo *root , int nivel){
